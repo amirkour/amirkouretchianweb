a14e7fad5d78eb1077d451f0250f13713791bc6d
Amir Kouretchian <amir.kouretchian@gmail.com>
2021-02-25 15:41:04 -0800

    dockerfile was missing the babelrc file, which caused docker builds to fail (with recent environment updates.) also updating some node dependencies, and some syntax in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 // exclude: /(node_modules|bower_components)/,
-                use: 'babel-loader'
+                loader: 'babel-loader'
             }
         ],
     },
